fdff80a2634e5a8f7251fd16f0fc610bde55d32e
abedmir31 <abedmir31@gmail.com>
2020-11-28 22:46:46 -0600

    update

--- spark.py
+++ spark.py
@@ -3,121 +3,52 @@ from pyspark.streaming import StreamingContext
 import googlemaps
 import re, string
 import random
-# from textblob import TextBlob
-# from elasticsearch import Elasticsearch
-
-
+import json
+from textblob import TextBlob
+from elasticsearch import Elasticsearch
 
 TCP_IP = 'localhost'
 TCP_PORT = 9001
 
-def processTweet(tweet):
+es = Elasticsearch()
 
-    # Here, you should implement:
-    # (i) Sentiment analysis,
-    # (ii) Get data corresponding to place where the tweet was generate (using geopy or googlemaps)
-    # (iii) Index the data using Elastic Search         
+def processTweet(data):
 
-    tweetData = tweet.split("::")
+         # Here, you should implement:
+         # (i) Sentiment analysis,
+         # (ii) Get data corresponding to place where the tweet was generate (using geopy or googlemaps)
+         # (iii) Index the data using Elastic Search      
+ 
+         dict_data = json.loads(data)
+         tweet = TextBlob(dict_data["text"])
+        
+         if tweet.sentiment.polarity < 0:
+            sentiment = "negative"
+         elif tweet.sentiment.polarity == 0:
+            sentiment = "neutral"
+         else:
+            sentiment = "positive"
 
-    if len(tweetData) > 1:
-        rawLocation = tweetData[0]
-        text = tweetData[1]
-        tokens = remove_noise(word_tokenize(text))
+         es.index(index="sentiment",
+                 doc_type="test-type",
+                 body={"author": dict_data["user"]["screen_name"],
+                       "date": dict_data["created_at"],
+                       "message": dict_data["text"],
+                       "polarity": tweet.sentiment.polarity,
+                       "subjectivity": tweet.sentiment.subjectivity,
+                       "sentiment": sentiment})
+
+         print("Tweet: ", tweet)
+         print("Sentiment: ", sentiment)
+         
+         # (i) Apply Sentiment analysis in "text"
+
+
+	 # (ii) Get geolocaton (state, country, lat, lon, etc...) from rawLocation
         
 
-        # (i) Apply Sentiment analysis in "text"
 
-
-	# (ii) Get geolocaton (state, country, lat, lon, etc...) from rawLocation
-        
-        print("\n\n=========================\ntweet: ", text, "\nlocation: ", rawLocation, "\nClassification: ", classifier.classify(dict([token, True] for token in tokens)), "\n=========================\n\n")
-
-
-        # (iii) Post the index on ElasticSearch or log your data in some other way (you are always free!!) 
-        
-#Sentiment training
-import nltk
-from nltk.corpus import twitter_samples
-from nltk.tag import pos_tag
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import stopwords
-from nltk import FreqDist
-from nltk import classify
-from nltk import NaiveBayesClassifier
-from nltk.tokenize import word_tokenize
-
-nltk.download('twitter_samples')
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
-positive_tweets = twitter_samples.strings('positive_tweets.json')
-negative_tweets = twitter_samples.strings('negative_tweets.json')
-text = twitter_samples.strings('tweets.20150430-223406.json') #TWEET
-tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
-stop_words = stopwords.words('english')
-
-positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
-
-positive_cleaned_tokens_list = []
-negative_cleaned_tokens_list = []
-
-def remove_noise(tweet_tokens, stop_words = ()):
-    cleaned_tokens = []
-
-    for token, tag in pos_tag(tweet_tokens):
-        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
-                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
-        token = re.sub("(@[A-Za-z0-9_]+)","", token)
-
-        if tag.startswith("NN"):
-            pos = 'n'
-        elif tag.startswith('VB'):
-            pos = 'v'
-        else:
-            pos = 'a'
-
-        lemmatizer = WordNetLemmatizer()
-        token = lemmatizer.lemmatize(token, pos)
-
-        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
-            cleaned_tokens.append(token.lower())
-    return cleaned_tokens
-
-for tokens in positive_tweet_tokens:
-    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
-
-for tokens in negative_tweet_tokens:
-    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
-
-def get_all_words(cleaned_tokens_list):
-    for tokens in cleaned_tokens_list:
-        for token in tokens:
-            yield token
-
-all_pos_words = get_all_words(positive_cleaned_tokens_list)
-freq_dist_pos = FreqDist(all_pos_words)
-
-def get_tweets_for_model(cleaned_tokens_list):
-    for tweet_tokens in cleaned_tokens_list:
-        yield dict([token, True] for token in tweet_tokens)
-
-positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
-positive_dataset = [(tweet_dict, "Positive")
-                     for tweet_dict in positive_tokens_for_model]
-negative_dataset = [(tweet_dict, "Negative")
-                     for tweet_dict in negative_tokens_for_model]
-
-dataset = positive_dataset + negative_dataset
-random.shuffle(dataset)
-train_data = dataset[:7000]
-test_data = dataset[7000:]
-
-classifier = NaiveBayesClassifier.train(train_data)
-
+         # (iii) Post the index on ElasticSearch or log your data in some other way (you are always free!!) 
 
 
 # Pyspark
@@ -136,10 +67,7 @@ ssc.checkpoint("checkpoint_TwitterApp")
 
 # read data from port 900
 dataStream = ssc.socketTextStream(TCP_IP, TCP_PORT)
-
-
 dataStream.foreachRDD(lambda rdd: rdd.foreach(processTweet))
-
 
 ssc.start()
 ssc.awaitTermination()
